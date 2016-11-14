8f65911f982264972c3ab6b7722498dd6ae47145
damoch <damochramowicz260@gmail.com>
2016-11-14 09:02:48 +0100

    Basic UI stuff

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+    private string v;
+
+    public Character(string name)
+    {
+        this.name = name;
+    }
+
+    public string Name { get; set; }
+    public float cash { get; set; }
+    public float tiredness { get; set; }
+    public float concetration { get; set; }
+    public float happiness { get; set; }
+
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+
+}
---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Controller : MonoBehaviour {
+    public DateTime time;
+
+    //public Character player = new Character("player1");
+
+	// Use this for initialization
+	void Start () {
+        //player = new Character("player1");
+        time = DateTime.Parse("1.01.2016 8:00");
+        Debug.Log(time.ToString());
+	}
+	
+    public void party()
+    {
+        Debug.Log("Having hella good time");
+    }
+
+    public void work()
+    {
+        Debug.Log("Working");
+    }
+
+    public void slackOff()
+    {
+        Debug.Log("Slacking off");
+    }
+    public void drink()
+    {
+        Debug.Log("Drinking lotta alcohol");
+    }
+
+    public void sleep()
+    {
+        Debug.Log("Goodnight");
+    }
+
+
+}
